a996aad9a8b31c8a3005e875fbaeeddeff71a090
Protsukovich Kirill <pkirill281999@gmail.com>
2019-03-14 22:54:25 +0300

    First commit

---
+++ FAPI/src/main/java/com/netcracker/edu/main/FapiApplication.java
@@ -0,0 +1,17 @@
+package com.netcracker.edu.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FapiApplication.class, args);
+
+
+    }
+
+}
+
